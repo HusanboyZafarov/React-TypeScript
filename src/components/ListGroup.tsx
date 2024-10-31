1ec8ec97e9537f850bd150a143b14ac152ad7188
Husanboy Zafarov <zhzr1222@gmail.com>
2024-10-31 13:58:29 +0500

    testing the git

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,7 +5,7 @@ const ListGroup = () => {
     <>
       <h1>Hello World</h1>
       <ul className='list-group'>
-        <li className='list-group-item'>An item</li>
+        <li className='list-group-item'></li>
         <li className='list-group-item'>A second item</li>
         <li className='list-group-item'>A third item</li>
         <li className='list-group-item'>A fourth item</li>
